54c18704ad1b227d70be27b3f3c1e6f1bc253089
Jason Urton <jason.urton@gmail.com>
2011-05-08 12:07:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grocerwords::Application.config.secret_token = 'b5af455a61e8a149de427c8b34e6acebcde17c7a5fce5e4b9e3343995667eef4779a902557b16b4179b4dfc286366987024fbb2741c564c9cc53dca1abedc1e8'
